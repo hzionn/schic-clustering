e8dea360cdd7d0cfdb2fbfd61392c4f6e3c09856
hzionn <hzionn@mac.local>
2024-11-27 04:35:51 +0800

    dev

---
+++ src/preprocessing/interactions_to_contact_map.py
@@ -0,0 +1,1 @@
+# TODO: convert bin-bin interactions into contact maps
---
+++ src/scripts/get_processed_review_data.sh
@@ -0,0 +1,4 @@
+cd data
+curl -O http://easybioai.com/scHIC_benchmark/Cell_ID.zip
+curl -O http://easybioai.com/scHIC_benchmark/Sparse_matrix.zip
+curl -O http://easybioai.com/scHIC_benchmark/The_embeddings.zip
